1ff80c2f66e96f7c885cb5695334c6038b600b43
AJBrachoRodriguez <alexangelb@gmail.com>
2024-08-24 20:18:46 -0400

    Updated of main and requirements and notebook.ipynb

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from fastapi import FastAPI
-from routes import router
+#from routes import router
+import pandas as pd
+import numpy as np
+import sklearn as sk
+import matplotlib as mpt
+
+
+global df_movies, df_ratings, df_final
+
+print("Done!")
